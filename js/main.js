229c5603b18f570a3a24d4b7d1d35952bfac9aa5
tomhoon <gnsdl9108@naver.com>
2025-07-01 19:50:37 +0900

    fix: 뒤로가기시 페이지 적용 안되는 현상

--- js/main.js
+++ js/main.js
@@ -29,6 +29,8 @@ const App = () => {
   const [path, setPath] = React.useState("");
   const [contentComponent, setContentComponent] = React.useState(<HomeMain />);
 
+  window.setPath = setPath;
+
   React.useEffect(() => {
     $R.pushPath({ pageName: path }, path);
 
@@ -76,3 +78,9 @@ window.$api_url =
     : "http://localhost:23000";
 
 console.log("window.location.hostname >>> ", window.location.hostname);
+
+window.addEventListener('popstate', e => {
+  const params = new URLSearchParams(location.href);
+  const pageName = params.get('pageName');
+  setPath(pageName || '');
+});
